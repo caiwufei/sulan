8a12789524de74abad6ba50620ff42beb3f7fc4b
蔡兀飞 <caiwufei@qtrade.com.cn>
2020-01-05 18:25:13 +0800

    提交代码

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+/*<![CDATA[*/
+contextPath = /*[[@{/}]]*/ '';
+/*]]>*/
+
+String.prototype.format = function () {
+    var values = arguments;
+    return this.replace(/\{(\d+)\}/g, function (match, index) {
+        if (values.length > index) {
+            return values[index];
+        } else {
+            return "";
+        }
+    });
+};
+
